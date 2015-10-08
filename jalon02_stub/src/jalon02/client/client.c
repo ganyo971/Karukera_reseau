d9252f7d8c663844ea3ee552b0af12dfcf459c0c
ganyo971 <ganyo_971@hotmail.fr>
2015-10-08 12:30:02 +0200

    Serveur multi fonctionnel manque juste à décréménter le nb_client à chaque /quit

--- jalon02_stub/src/jalon02/client/client.c
+++ jalon02_stub/src/jalon02/client/client.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <stddef.h>
 
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -59,6 +60,12 @@ int main(int argc,char** argv)
         return 1;
     }
 
+// File descriptor set for the select
+    fd_set readfds;
+    fd_set writefds;
+    fd_set exceptfds;
+    struct timeval timeout;
+
 //get address info from the server
     struct addrinfo addr;
     struct addrinfo * add = &addr;
@@ -76,14 +83,19 @@ int main(int argc,char** argv)
     do_connect(sockfd,addr.ai_addr, addr.ai_addrlen);
 
     for(;;){
+
+
+        FD_SET(sockfd, &writefds);
+
         char buffer[256];
         memset(&buffer, 0, 256);
-
         fprintf (stdout,"Tapez le message à envoyer (moins de 256 caractères) : \n");
         fgets((char *) buffer, 256, stdin);
-        int ecrit = write(sockfd, buffer, strlen((char *) buffer));
-                
+        write(sockfd, buffer, strlen((char *) buffer));
+
         //handle_client_message(sockfd, &buffer, (int) 256);
+
+        FD_SET(sockfd, &readfds);
 
         memset(&buffer, 0, (int) 256);
         do_read(sockfd, buffer);
@@ -92,6 +104,8 @@ int main(int argc,char** argv)
             fprintf(stdout, "Connection terminated\n");
             break;
         }
+
+   
     }
 
     close(sockfd);
--- jalon02_stub/src/jalon02/server/server_app.c
+++ jalon02_stub/src/jalon02/server/server_app.c
@@ -40,11 +40,16 @@ int main(int argc, char** argv)
     struct sockaddr_in * serv_addr = malloc(sizeof(*serv_addr));
     struct sockaddr_in * addr = malloc(sizeof(*addr));
 
+    int max(int a, int b)   {
+
+        int max = a>b ? a:b;
+        return max;
+    }
+
 
     //create the socket, check for validity!
     sockfd = do_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-
+    
     //init the serv_add structure
     init_serv_addr(argv[1],serv_addr);
 
@@ -53,40 +58,124 @@ int main(int argc, char** argv)
     do_bind(sockfd, serv_addr, sizeof(*serv_addr));
 
     //specify the socket to be a server socket and listen for at most 20 concurrent client
-    if(listen(sockfd, 20) == -1) 
-        error("ERROR Listen");
+    if(listen(sockfd, 4) == -1) 
+        error("ERROR Listen, trop de connexion");
 
-    int new_socks;
-    //accept connection from client
-    new_socks = do_accept(sockfd, (struct sockaddr *) addr, (socklen_t) sizeof(addr));
+    //Tableau avec les numeros de socket client
+    int socks[20];
+    memset(socks, 0, 20);
+
+    //Nombre de client connecté
+    int nb_client = 0;
+
+    //init the fdset
+    int nfds = 0;
+    fd_set readfds;
+    fd_set rd_select;
+    fd_set writefds;
+    fd_set exceptfds;
+    struct timeval timeout;
+
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+
+    FD_ZERO(&readfds);
+    FD_ZERO(&rd_select);
+    FD_ZERO(&writefds);
+    FD_ZERO(&exceptfds);
+
+    // Surveiller la socket du server pour la lecture 
+    FD_SET(sockfd, &readfds);
+    nfds = max(nfds, sockfd);
+    
+    //in an infinite loop, listen to the stored sockets
 
     for (;;){
-        //read what the client has to say
-        char buffer[256];
-        memset(buffer, 0, 256);
-        int lu = do_read(new_socks, buffer);            
+       
+        //use select to listen to sockets
 
-        
-        if (strcmp(buffer,"/quit\n") == 0)  {
+        //FD_COPY(&readfds, &rd_select);
 
-            fprintf(stdout, "[CLIENT] : %s", buffer);
-            do_write(new_socks, "You will be terminated\n");
-            break;
+        fd_set* p = &rd_select ;
+
+        *p = readfds;
+
+        int sel = select(nfds + 1, &rd_select, NULL, NULL, NULL);
+
+        //printf("Valeur de select : %d", sel);
+
+        if (sel == -1)     {
+            perror("select()");
+            exit(EXIT_FAILURE);
         }
 
-        else    {
+        //implement the listening for incoming sockets
+
+         if (FD_ISSET(sockfd, &rd_select)) {
             
-            fprintf(stdout, "[CLIENT] : %s", buffer);
+            if (nb_client+1 > 2)    {
+                int sock_tmp = do_accept(sockfd, (struct sockaddr *) addr, (socklen_t) sizeof(addr));
 
-            //we write back to the client
-            int e = do_write(new_socks, buffer);            
+                
+                char* tmp = "Can not accept further connexion: try later\n";
+                
+                do_write(sock_tmp, tmp);
+                close(sock_tmp); 
+                break;
+            }
+        //accept connection from client
+            else    {
+           
+                socks[nb_client] = do_accept(sockfd, (struct sockaddr *) addr, (socklen_t) sizeof(addr));            
+                FD_SET(socks[nb_client], &readfds);
+                nfds = max(nfds, socks[nb_client]);
+                nb_client ++;
+            }
         }
         
-    }
+        // then implement the listening for already connected socket that write data to the server
+  
+        /*int new_socks;
+        //accept connection from client
+            new_socks = do_accept(sockfd, (struct sockaddr *) addr, (socklen_t) sizeof(addr));*/
+        int j;
 
+        for (j = 0; j < nb_client; j++)     {
 
+            if (FD_ISSET(socks[j], &rd_select))     {
+
+                //read what the client has to say
+                
+                char buffer[256];
+                memset(buffer, 0, 256);
+                do_read(socks[j], buffer);        
+                
+                if (strcmp(buffer,"/quit\n") == 0)  {
+
+                    fprintf(stdout, "[CLIENT n°%i] : %s", j, buffer);
+
+                    do_write(socks[j], "You will be terminated\n");
+                    break;
+                }
+
+                else    {
+                    
+                    fprintf(stdout, "[CLIENT] : %s", buffer);
+
+                    //we write back to the client
+                    do_write(socks[j], buffer);            
+                }
+            }
+        }
+       
+    } 
+
+    int j;
+
+    for (j = 0; j < nb_client; j++)     {  
     //clean up client socket
-    close(new_socks);
+        close(socks[j]);
+    }
 
     //clean up server socket
     close(sockfd);
